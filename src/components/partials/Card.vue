0b959caae4fab8aba863da4938100b7ab406bb00
marroccuandrea <marroccuandrea@yahoo.it>
2024-04-04 15:01:50 +0200

    Milestone 3 non completata

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -1,5 +1,5 @@
 <script>
-// import { store } from '../../data/store'
+import { store } from '../../data/store'
 export default {
     props: {
         title: String,
@@ -8,11 +8,38 @@ export default {
         original_language: String,
         vote_average: Number
     },
-    // data() {
-    //     return {
-    //         store
-    //     }
-    // }
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        stars(rating) {
+            let star = '';
+            const vote = rating / 2;
+            const stelle = Math.floor(vote);
+            const mezzaStella = vote % 1 >= 0.5;
+
+
+            // Stelle piene
+            for (let i = 0; i < stelle; i++) {
+                star += `<i class="fa-solid fa-star"></i>`;
+                // Mezza stella
+            }
+            if (mezzaStella) {
+                star += `<i class="fa-regular fa-star-half-stroke"></i>`
+            }
+
+            // Stelle vuote
+            const stellaVuota = 5;
+            for (let i = 0; i < stelle; i++) {
+
+            } if (stellaVuota) {
+                star += `<i class="fa-regular fa-star"></i>`;
+            }
+            return star;
+        }
+    }
 }
 </script>
 
@@ -26,7 +53,7 @@ export default {
                 <img v-if="original_language == 'it'" src="../../assets/bandiere/it.png"></img>
                 <img v-else-if="original_language == 'en'" src="../../assets/bandiere/en.png"></img>
                 <p v-else class="card-text">{{ original_language }}</p>
-                <p class="card-text">{{ vote_average }}</p>
+                <p v-html="stars(vote_average)" class="card-text"></p>
             </div>
         </div>
     </div>
