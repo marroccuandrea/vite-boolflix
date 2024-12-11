5e40b35f0c3fa79263fe31827af3a37be6885001
marroccuandrea <marroccuandrea@yahoo.it>
2024-12-11 19:01:04 +0100

    [add] most popular tv series & movies

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,56 @@
 <script>
-  import axios from 'axios';
-  import { store } from './data/store.js'
-  import Header from './components/Header.vue'
-  import Main from './components/Main.vue'
-  export default {
-    components:{
-      Header,
-      Main
-    },
-    data(){
-      return{
-        store
-      }
-    },
-    methods:{
-      getMovies(){
-        axios.get(this.store.moviesApi, {
-          params: store.queryParams
+import axios from "axios";
+import { store } from "./data/store.js";
+import { onMounted } from "vue";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+export default {
+  components: {
+    Header,
+    Main,
+  },
+  setup() {
+    onMounted(() => {
+      store.fetchPopularContent();
+    });
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getMovies() {
+      axios
+        .get(this.store.moviesApi, {
+          params: store.queryParams,
         })
-        .then(result => {
-          this.store.moviesList = result.data.results
+        .then((result) => {
+          this.store.moviesList = result.data.results;
           console.log(this.store.moviesList);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
+        });
+    },
+    getSeries() {
+      axios
+        .get(this.store.seriesApi, {
+          params: store.queryParams,
         })
-      },
-      getSeries(){
-        axios.get(this.store.seriesApi, {
-          params: store.queryParams
-        })
-        .then(result => {
-          this.store.seriesList = result.data.results
+        .then((result) => {
+          this.store.seriesList = result.data.results;
           console.log(this.store.seriesList);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
-        })
-      }
+        });
     },
-    mounted(){
-      this.getMovies(),
-      this.getSeries()
-    }
-  }
+  },
+  mounted() {
+    this.getMovies(), this.getSeries();
+  },
+};
 </script>
 
 <template>
@@ -52,5 +59,5 @@
 </template>
 
 <style lang="scss">
-@use './assets/scss/main.scss';
+@use "./assets/scss/main.scss";
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -1,15 +1,47 @@
-import { reactive } from 'vue'
+// Replica di Netflix con funzionalità di ricerca e pagina iniziale con film/serie popolari
+
+import { reactive } from "vue";
 
 export const store = reactive({
-    mainTitle: 'Vite Boolix',
-    moviesApi: 'https://api.themoviedb.org/3/search/movie',
-    seriesApi: 'https://api.themoviedb.org/3/search/tv',
-    cardsList: [],
-    moviesList: [],
-    seriesList: [],
-    queryParams: {
-        api_key: '22a1005abcd5268fa491114e2d41ca28',
-        query: '',
-        language: 'it-IT'
+  mainTitle: "Vite Boolix",
+  moviesApi: "https://api.themoviedb.org/3/search/movie",
+  seriesApi: "https://api.themoviedb.org/3/search/tv",
+  seriesPopular: "https://api.themoviedb.org/3/tv/popular",
+  moviesPopular: "https://api.themoviedb.org/3/movie/popular",
+  cardsList: [],
+  moviesList: [],
+  seriesList: [],
+  queryParams: {
+    api_key: "22a1005abcd5268fa491114e2d41ca28",
+    query: "",
+    language: "it-IT",
+  },
+
+  // Funzione per recuperare film e serie popolari all'avvio
+  async fetchPopularContent() {
+    try {
+      // Recupera film popolari
+      const moviesResponse = await fetch(
+        `${this.moviesPopular}?api_key=${this.queryParams.api_key}&language=${this.queryParams.language}`
+      );
+      if (!moviesResponse.ok)
+        throw new Error("Errore nel caricamento dei film popolari");
+      const moviesData = await moviesResponse.json();
+      this.moviesList = moviesData.results;
+
+      // Recupera serie TV popolari
+      const seriesResponse = await fetch(
+        `${this.seriesPopular}?api_key=${this.queryParams.api_key}&language=${this.queryParams.language}`
+      );
+      if (!seriesResponse.ok)
+        throw new Error("Errore nel caricamento delle serie popolari");
+      const seriesData = await seriesResponse.json();
+      this.seriesList = seriesData.results;
+    } catch (error) {
+      console.error(
+        "Errore durante il caricamento dei contenuti popolari:",
+        error
+      );
     }
-})
+  },
+});
